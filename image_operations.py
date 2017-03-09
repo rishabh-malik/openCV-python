aa4aaa878cc216a71aed7a62126045a6e82ef4b8
rishabh-malik <rishabhmalik249@gmail.com>
2017-03-09 20:59:28 -0300

    initial commit

---
+++ image_operations.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img=cv2.imread('pic.jpg',cv2.IMREAD_COLOR)
+
+px=img[55,55]
+print(px)                  #to print the color value of that co ordinate
+
+img[55,55]=[255,255,0]     #changing the color of that coordinate
+print(px)
+
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
