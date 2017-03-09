d2bac5b18d5a42a6f371fc7930944ecde6ef2cd9
rishabh-malik <rishabhmalik249@gmail.com>
2017-03-09 20:03:51 -0300

    added comments

--- loading-videos.py
+++ loading-videos.py
@@ -2,10 +2,11 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-cap=cv2.VideoCapture(0)
+cap=cv2.VideoCapture(0)     #to capture the video through webcam
+
 while True:
     ret, frame=cap.read()
-    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)  #for showing the video in gray mode
     cv2.imshow('Webcam',frame)
     cv2.imshow('Webcam gray',gray)
 
