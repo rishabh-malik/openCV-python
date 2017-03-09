9044b0514ee52f6d1214aeda3ac5709db9875c2e
rishabh-malik <rishabhmalik249@gmail.com>
2017-03-09 19:59:53 -0300

    ADDED video in gray mode

--- loading-videos.py
+++ loading-videos.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 cap=cv2.VideoCapture(0)
 while True:
     ret, frame=cap.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     cv2.imshow('Webcam',frame)
+    cv2.imshow('Webcam gray',gray)
 
     if cv2.waitKey(1) & 0xFF==ord('q'):     #if we press q then the infinite loop breaks
         break
