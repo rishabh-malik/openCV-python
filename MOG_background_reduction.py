536144d9ede9c693bad366405ab3d6fc3cb03d8a
rishabh-malik <rishabhmalik249@gmail.com>
2017-03-10 19:13:37 -0300

    extracting action

--- MOG_background_reduction.py
+++ MOG_background_reduction.py
@@ -1,6 +1,23 @@
 #getting rid of background to extract foreground
 #used only for extracting action from videos
+#currently working only for webcam not for video loaded 
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+cap=cv2.VideoCapture('people-walking.mp4')
+fgbg=cv2.createBackgroundSubtractorMOG2()       #for creating background
+
+while True:
+    ret,frame=cap.read()
+    fgmask=fgbg.apply(frame)                    #apply mask to video
+
+    cv2.imshow('original',frame)
+    cv2.imshow('fg',fgmask)
+
+    k=cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()        
