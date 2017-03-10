9682edcd1b8d3205b7926db56fbac67c279a785c
rishabh-malik <rishabhmalik249@gmail.com>
2017-03-09 21:06:26 -0300

    added copy pasting and region of space concept

--- image_operations.py
+++ image_operations.py
@@ -10,6 +10,11 @@ print(px)                  #to print the color value of that co ordinate
 img[55,55]=[255,255,0]     #changing the color of that coordinate
 print(px)
 
+img[100:150,100:150]=[255,255,255]       #changing that image portion to white-ROI(region of image)
+
+face=img[40:100,50:100]                  #copy and pasting regions of space
+img[0:60,0:50]=face
+
 cv2.imshow('img',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
