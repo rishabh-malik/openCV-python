c289405e78494f2ad28d23dc0bc81ec32108dfa9
rishabh-malik <rishabhmalik249@gmail.com>
2017-03-09 22:33:01 -0300

    basic

--- thresholding.py
+++ thresholding.py
@@ -1,6 +1,13 @@
 #simplifying the images in the form of 0 and 1
+#made the image bright and readable
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
 img=cv2.imread('bookpage.jpg')
+retval,threshold=cv2.threshold(img,12,255,cv2.THRESH_BINARY)         #applying threshold to image, value  above 12 is white and below that is black
+
+cv2.imshow('original',img)
+cv2.imshow('threshold image',threshold)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
