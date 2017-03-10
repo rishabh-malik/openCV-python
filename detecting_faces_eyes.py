de5b442718d66aa576f6cce98045075210991ce9
rishabh-malik <rishabhmalik249@gmail.com>
2017-03-10 20:40:59 -0300

    default cascade

--- detecting_faces_eyes.py
+++ detecting_faces_eyes.py
@@ -13,7 +13,7 @@ cap=cv2.VideoCapture(0)     #to capture the video through webcam
 while True:
     ret, img=cap.read()
     gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    faces=face_cascade.detectMultiScale(gray,1.3,5)                    #detecting the face in gray since it is easy to detect in gray
+    faces=face_cascade.detectMultiScale(gray)                    #detecting the face in gray since it is easy to detect in gray
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray=gray[y:y+h,x:x+h]                                     #a region of space in the face
