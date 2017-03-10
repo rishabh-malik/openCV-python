cc9be5ef1ffe69e37ae231809c3bbf2bb7cd54f5
rishabh-malik <rishabhmalik249@gmail.com>
2017-03-10 17:55:24 -0300

    corners tracked

--- corner_detection.py
+++ corner_detection.py
@@ -8,4 +8,12 @@ gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 gray=np.float32(gray)
 
 corners=cv2.goodFeaturesToTrack(gray,100,0.01,10)        #no of corners, image quality, min distance b/w corners
+corners=np.int0(corners)
 
+for corner in corners:
+    x,y=corner.ravel()                                  #circling the corners
+    cv2.circle(img,(x,y),3,255,-1)
+
+cv2.imshow('corners detected',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
