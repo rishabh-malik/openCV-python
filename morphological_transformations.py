e65784f8e1c5f7af8e6fe1249e82473c9de5c54b
rishabh-malik <rishabhmalik249@gmail.com>
2017-03-10 16:56:02 -0300

    opening and closing

--- morphological_transformations.py
+++ morphological_transformations.py
@@ -22,11 +22,20 @@ while True:
     kernel=np.ones((5,5),np.uint8)
     erosion=cv2.erode(mask,kernel,iterations=1)
     dilation=cv2.dilate(mask,kernel,iterations=0)
+
+    #opening- removes false positives
+    #closing- removes false negative
+
+    opening= cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernel)
+    closing= cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernel)
+    
     
     cv2.imshow('frame',frame)                  
     cv2.imshow('res',res)
     cv2.imshow('erosion',erosion)                  
     cv2.imshow('dilation',dilation)
+    cv2.imshow('opening',opening)                  
+    cv2.imshow('closing',closing)
 
     k=cv2.waitKey(5) & 0xFF
     if k==27:                                                        #escape to quit
